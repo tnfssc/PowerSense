db272565db2f0f8015b9b4f1fddc23991d582270
tnfssc <tnfssc@gmail.com>
2022-01-18 03:42:35 +0530

    TS issue fix

--- api-utils/twilio/index.ts
+++ api-utils/twilio/index.ts
@@ -1,8 +1,7 @@
-import twilio from "twilio";
+import { Twilio } from "twilio";
 
-const ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
-const AUTH_TOKEN = process.env.TWILIO_AUTHTOKEN;
-
-const twilioClient = twilio(ACCOUNT_SID, AUTH_TOKEN);
+const ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID!;
+const AUTH_TOKEN = process.env.TWILIO_AUTHTOKEN!;
+const twilioClient = new Twilio(ACCOUNT_SID, AUTH_TOKEN);
 
 export default twilioClient;

637ef8602a7511d966125af89eec03178f0f9bbf
tnfssc <tnfssc@gmail.com>
2022-01-22 20:07:32 +0530

    Phone changed in Profile

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,12 +1,14 @@
-import { Flex, Heading, CircularProgress, useToast } from "@chakra-ui/react";
+import { Flex, Heading, CircularProgress, useToast, Button } from "@chakra-ui/react";
 import * as yup from "yup";
 import { Formik, Form } from "formik";
 import { InputControl, SubmitButton } from "formik-chakra-ui";
 
 import useAuth from "../../use/auth";
+import usePhones from "../../use/phones";
 import useProfile, { ProfileType } from "../../use/profile";
 
 import "./index.css";
+import Link from "../../components/Link";
 
 const validationSchema = yup.object({
   id: yup.string().required("ID is required"),
@@ -20,7 +22,7 @@ const validationSchema = yup.object({
 });
 
 export default function Profile() {
-  const user = useAuth();
+  const user = useAuth()!;
   const toast = useToast();
   const {
     profile: { data: profile, isLoading },
@@ -45,12 +47,18 @@ export default function Profile() {
       },
     });
   };
+  const { phone } = usePhones(user.id);
 
   if (isLoading) return <CircularProgress isIndeterminate />;
   return (
     <Flex flexDir="column" alignItems="center" w="full">
       <Heading>Profile Page</Heading>
       {user?.email?.endsWith("@iith.ac.in") ? <Heading size="md">You belong to IITH</Heading> : <></>}
+      <Flex w="full" justifyContent="flex-end">
+        <Link to="/verify-phone">
+          <Button isLoading={phone.isLoading}>{phone.data?.phone ?? "Link phone"}</Button>
+        </Link>
+      </Flex>
       <Formik initialValues={profile!} validationSchema={validationSchema} onSubmit={handleSubmit}>
         {({ handleSubmit }) => (
           <Form onSubmit={handleSubmit} className="form">

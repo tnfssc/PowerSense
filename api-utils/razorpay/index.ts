7ee4ff2eda80d174c564ebe003e0e7ec50b2f7b2
tnfssc <tnfssc@gmail.com>
2022-02-03 21:32:28 +0530

    Fix RPay

--- api-utils/razorpay/index.ts
+++ api-utils/razorpay/index.ts
@@ -2,6 +2,9 @@ import Razorpay from "razorpay";
 
 const RAZORPAY_KEYID = process.env.RAZORPAY_KEYID!;
 const RAZORPAY_KEY_SECRET = process.env.RAZORPAY_KEY_SECRET!;
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 const razorpay = new Razorpay({
   key_id: RAZORPAY_KEYID,
   key_secret: RAZORPAY_KEY_SECRET,

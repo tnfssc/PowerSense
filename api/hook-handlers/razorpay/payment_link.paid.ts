16b328b5cef0dadbacb032d116efd0eb5741d56f
tnfssc <tnfssc@gmail.com>
2022-02-03 19:49:46 +0530

    Test pay webhook

--- api/hook-handlers/razorpay/payment_link.paid.ts
+++ api/hook-handlers/razorpay/payment_link.paid.ts
@@ -1,9 +1,18 @@
 import type { VercelApiHandler } from "@vercel/node";
-// import supabase from "../../../api-utils/supabase";
+import type { RazorpayRequest } from "../../types/razorpay";
+import type { CourseRegistrations } from "../../types/supabase/tables";
+import supabase from "../../../api-utils/supabase";
 
 const handler: VercelApiHandler = async (req, res) => {
   try {
-    console.log(JSON.stringify(req.body, null, 2));
+    const [user_id, course_id] = (req.body as RazorpayRequest).payload.payment_link.entity.reference_id.split("+");
+    const { error } = await supabase
+      .from<CourseRegistrations>("course-registrations")
+      .update({ paid: true })
+      .eq("user_id", user_id)
+      .eq("course_id", course_id);
+    if (error) return res.status(500).json({ error });
+    return res.status(200).json({ success: true });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error });
---
+++ api/types/supabase/tables.d.ts
@@ -0,0 +1,6 @@
+export interface CourseRegistrations {
+  user_id: string;
+  course_id: number;
+  registered_at: number;
+  paid: boolean;
+}
